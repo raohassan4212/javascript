ca55135a73d16af15bf4721d77f5c9ae3cf3eca6
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2022-09-05 23:12:20 +0500

    practice

--- index.js
+++ index.js
@@ -77,26 +77,26 @@
 //   [7, 8, 9],
 // ];
 
-// let arr1 = [1, 2, 3, 4, 5];
-// let arr2 = [3, 4, 5, 6, 7];
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [3, 4, 5, 6, 7];
 
-// let intersections = []
+let intersections = []
 
-// for (var i = 0; i < arr1.length; i++) {
-//     for (var j = 0; j < arr2.length; j++) {
-//         if (arr1[i] === arr2[j]) {
-//             intersection.push(arr1[i]);
-//         }
-//     }
-// }
+for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+        if (arr1[i] === arr2[j]) {
+            intersection.push(arr1[i]);
+        }
+    }
+}
 
-// console.log(intersection)
+console.log(intersection)
 
-// let intersection = arr1.filter((val) => {
-//     return arr2.includes(val)
-// })
+let intersection = arr1.filter((val) => {
+    return arr2.includes(val)
+})
 
-// console.log(intersection)
+console.log(intersection)
 
 let union = [...new Set([...arr1, ...arr2])];
 console.log(union);
